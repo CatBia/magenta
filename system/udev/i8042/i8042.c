AlexxNica/magenta-1

load i8042 from MDI
Change-Id: I9c0ca924abd1f20c76a20c871c822f8e29039803

system/udev/i8042/i8042.c
@@ -826,6 +826,9 @@ static mx_driver_ops_t i8042_driver_ops = {
 };
 
 //TODO: should bind against PC/ACPI instead of misc
-MAGENTA_DRIVER_BEGIN(i8042, i8042_driver_ops, "magenta", "0.1", 1)
-    BI_MATCH_IF(EQ, BIND_PROTOCOL, MX_PROTOCOL_MISC_PARENT),
+MAGENTA_DRIVER_BEGIN(i8042, i8042_driver_ops, "magenta", "0.1", 4)
+    BI_ABORT_IF(NE, BIND_PROTOCOL, MX_PROTOCOL_SOC),
+    BI_ABORT_IF(NE, BIND_SOC_VID, 12),
+    BI_ABORT_IF(NE, BIND_SOC_PID, 23),
+    BI_MATCH_IF(EQ, BIND_SOC_DID, 34),
 MAGENTA_DRIVER_END(i8042)

CatBia/magenta

system/udev/i8042/i8042.c
@@ -826,6 +826,9 @@ static mx_driver_ops_t i8042_driver_ops = {
 };
 
 //TODO: should bind against PC/ACPI instead of misc
-MAGENTA_DRIVER_BEGIN(i8042, i8042_driver_ops, "magenta", "0.1", 1)
-    BI_MATCH_IF(EQ, BIND_PROTOCOL, MX_PROTOCOL_MISC_PARENT),
+MAGENTA_DRIVER_BEGIN(i8042, i8042_driver_ops, "magenta", "0.1", 4)
+    BI_ABORT_IF(NE, BIND_PROTOCOL, MX_PROTOCOL_PLATFORM_DEV),
+    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_VID, 12),
+    BI_ABORT_IF(NE, BIND_PLATFORM_DEV_PID, 23),
+    BI_MATCH_IF(EQ, BIND_PLATFORM_DEV_DID, 34),
 MAGENTA_DRIVER_END(i8042)